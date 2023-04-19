b7e0e4143465b0917ab2cad7a3fb4550f411f930
david <davidmatovu88@gmail.com>
2023-04-19 09:36:57 +0300

    display meals on a meals screen

--- meals-app/screens/MealsOverviewScreen.js
+++ meals-app/screens/MealsOverviewScreen.js
@@ -1,16 +1,30 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet, Text } from "react-native";
 import { useRoute } from "@react-navigation/native"; // alternative of route
 
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 // get a route prop in a component registered as a screen in react navigation
 function MealsOverviewScreen({ route }) {
   // const route = useRoute()
   // const cat = route.params.categoryId
   const categoryId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen {categoryId}</Text>
+      {/* <Text>Meals Overview Screen {categoryId}</Text> */}
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
