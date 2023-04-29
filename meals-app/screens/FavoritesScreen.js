1bf8cdcf89bb349c8a6fc994554db4b135c0c3a4
david <davidmatovu88@gmail.com>
2023-04-29 13:07:41 +0300

    manage redux state and dispatch actions

--- meals-app/screens/FavoritesScreen.js
+++ meals-app/screens/FavoritesScreen.js
@@ -1,24 +1,27 @@
-import React, { useContext } from 'react'
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 
+import { View, Text, StyleSheet } from "react-native";
 
-import MealsList from '../components/MealsList/MealsList'
-import { FavoriteContext } from '../store/context/Favarites-context'
-import { MEALS } from '../data/dummy-data';
+import MealsList from "../components/MealsList/MealsList";
+// import { FavoriteContext } from '../store/context/Favarites-context'
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-  const favoriteMealsContext = useContext(FavoriteContext);
+  // const favoriteMealsContext = useContext(FavoriteContext);
+  const favoriteMeals = useSelector((state) => state.favoriteMeals.ids);
 
-  const favoriteMeal = MEALS.filter(meal =>favoriteMealsContext.ids.includes(meal.id))
+  // const favoriteMeal = MEALS.filter(meal =>favoriteMealsContext.ids.includes(meal.id))
+  const favoriteMeal = MEALS.filter((meal) => favoriteMeals.includes(meal.id));
 
-  if(favoriteMeal.length === 0){
-    return <View style={styles.rootContainer}>
-      <Text style={styles.text}>You have no favorite meals yet</Text>
-    </View>
+  if (favoriteMeal.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet</Text>
+      </View>
+    );
   }
-  return (
-    <MealsList items={favoriteMeal}/>
-  )
+  return <MealsList items={favoriteMeal} />;
 }
 
 export default FavoritesScreen;
@@ -32,6 +35,6 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 18,
     fontWeight: "bold",
-    color: "white"
-  }
-})
+    color: "white",
+  },
+});
